082fb505e754eac397113e8562bc8cd36b2607c5
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2021-05-19 16:34:17 +0200

    feat: add bootstrap 5 to Nextouch theme

---
+++ app/design/frontend/Devmy/nextouch/Magento_Theme/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+        'bootstrap': 'Magento_Theme/js/vendor/bootstrap.bundle',
+    },
+    shim: {
+        'bootstrap': {
+            'deps': ['jquery']
+        }
+    }
+};
