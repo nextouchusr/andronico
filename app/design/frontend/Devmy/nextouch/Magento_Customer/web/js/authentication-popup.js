1bc4048744e73c8477537e2069c9cf6ec3634b2b
Antonio Carboni <bitbucket@antoniocarboni.com>
2022-03-18 13:43:31 +0100

    add login popup

---
+++ app/design/frontend/Devmy/nextouch/Magento_Customer/web/js/authentication-popup.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal',
+    'mage/url'
+], function ($, modal,url) {
+    'use strict';
+
+    return {
+        modalWindow: null,
+
+        /**
+         * Create popUp window for provided element
+         *
+         * @param {HTMLElement} element
+         */
+        createPopUp: function (element) {
+            var options = {
+                'type': 'popup',
+                'modalClass': 'popup-authentication',
+                'focus': '[name=username]',
+                'responsive': true,
+                'innerScroll': true,
+                'trigger': '.proceed-to-checkout',
+                'buttons': []
+            };
+
+            this.modalWindow = element;
+            modal(options, $(this.modalWindow));
+        },
+
+        getBaseUrl: function(u = '') {
+            return url.build(u);
+        },
+
+        /** Show login popup window */
+        showModal: function () {
+            $(this.modalWindow).modal('openModal').trigger('contentUpdated');
+        }
+
+    };
+});
