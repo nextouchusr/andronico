66563ccf28f33ff66e98660fe42b3c48fcc6d019
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-03-24 13:03:34 +0100

    feat: group attributes by group name

---
+++ app/code/Nextouch/Theme/Block/Catalog/Product/View/Attributes.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace Nextouch\Theme\Block\Catalog\Product\View;
+
+use Magento\Framework\Phrase;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template\Context;
+use Nextouch\Eav\Api\AttributeGroupRepositoryInterface;
+
+class Attributes extends \Magento\Catalog\Block\Product\View\Attributes
+{
+    private AttributeGroupRepositoryInterface $attributeGroupRepository;
+
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        PriceCurrencyInterface $priceCurrency,
+        AttributeGroupRepositoryInterface $attributeGroupRepository,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $priceCurrency, $data);
+        $this->attributeGroupRepository = $attributeGroupRepository;
+    }
+
+    public function getAdditionalData(array $excludeAttr = []): array
+    {
+        $data = [];
+        $product = $this->getProduct();
+        $attributeSetId = (int) $product->getAttributeSetId();
+        $attributeGroups = $this->attributeGroupRepository->getItemsByAttributeSetId($attributeSetId);
+
+        foreach ($attributeGroups as $attributeGroup) {
+            $attributes = $product->getAttributes($attributeGroup->getAttributeGroupId());
+
+            foreach ($attributes as $attribute) {
+                if ($this->isVisibleOnFrontend($attribute, $excludeAttr)) {
+                    $value = $attribute->getFrontend()->getValue($product);
+
+                    if ($value instanceof Phrase) {
+                        $value = (string) $value;
+                    } elseif ($attribute->getFrontendInput() == 'price' && is_string($value)) {
+                        $value = $this->priceCurrency->convertAndFormat($value);
+                    }
+
+                    if (is_string($value) && strlen(trim($value))) {
+                        $data[$attributeGroup->getAttributeGroupId()] = [
+                            'label' => $attributeGroup->getAttributeGroupName(),
+                            'attributes' => [
+                                $attribute->getAttributeCode() => [
+                                    'label' => $attribute->getStoreLabel(),
+                                    'value' => $value,
+                                    'code' => $attribute->getAttributeCode(),
+                                ],
+                            ],
+                        ];
+                    }
+                }
+            }
+        }
+
+        return $data;
+    }
+}
