85750cb5ab481233f3d1b00ea54ba84d542380ee
Antonio Carboni <bitbucket@antoniocarboni.com>
2022-03-28 16:44:49 +0200

    change cat menu

--- app/code/Nextouch/Theme/Block/Html/Topmenu.php
+++ app/code/Nextouch/Theme/Block/Html/Topmenu.php
@@ -65,6 +65,7 @@ class Topmenu extends \Magento\Theme\Block\Html\Topmenu
             $colStops = $this->_columnBrake($child->getChildren(), $limit);
         }
 
+
         $html .= '<ul class="level' . $childLevel . ' ' . $childrenWrapClass . '">';
         $html .= '<li class="level' . $childLevel . ' level-back "><span>';
         $html .= $this->escapeHtml($child->getName()) . '</span></li>';
@@ -72,5 +73,45 @@ class Topmenu extends \Magento\Theme\Block\Html\Topmenu
         $html .= '</ul>';
 
         return $html;
+    }
+
+    protected function _getMenuItemClasses(Node $item)
+    {
+        $classes = [
+            'level' . $item->getLevel(),
+            $item->getPositionClass(),
+        ];
+
+        if($item->getLevel() == 0) {
+            $classes[] =  $item->getId();
+        }
+
+        if ($item->getIsCategory()) {
+            $classes[] = 'category-item';
+        }
+
+        if ($item->getIsFirst()) {
+            $classes[] = 'first';
+        }
+
+        if ($item->getIsActive()) {
+            $classes[] = 'active';
+        } elseif ($item->getHasActive()) {
+            $classes[] = 'has-active';
+        }
+
+        if ($item->getIsLast()) {
+            $classes[] = 'last';
+        }
+
+        if ($item->getClass()) {
+            $classes[] = $item->getClass();
+        }
+
+        if ($item->hasChildren()) {
+            $classes[] = 'parent';
+        }
+
+        return $classes;
     }
 }
