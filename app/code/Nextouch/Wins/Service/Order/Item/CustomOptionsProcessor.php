d3f87c99e93a0e084d51f18e2f058ef53dc747c7
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-04-08 09:52:56 +0200

    feat: add reference_item_id to order item

--- app/code/Nextouch/Wins/Service/Order/Item/CustomOptionsProcessor.php
+++ app/code/Nextouch/Wins/Service/Order/Item/CustomOptionsProcessor.php
@@ -5,6 +5,7 @@ namespace Nextouch\Wins\Service\Order\Item;
 
 use Magento\Catalog\Api\Data\CustomOptionInterface;
 use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderItemExtensionFactory;
 use Magento\Sales\Api\Data\OrderItemInterface;
 use Magento\Sales\Api\Data\OrderItemInterfaceFactory;
 use function Lambdish\Phunctional\map;
@@ -15,10 +16,14 @@ class CustomOptionsProcessor
     private const OPTION_SEPARATOR = ',';
 
     private OrderItemInterfaceFactory $orderItemInterfaceFactory;
+    private OrderItemExtensionFactory $orderItemExtensionFactory;
 
-    public function __construct(OrderItemInterfaceFactory $orderItemInterfaceFactory)
-    {
+    public function __construct(
+        OrderItemInterfaceFactory $orderItemInterfaceFactory,
+        OrderItemExtensionFactory $orderItemExtensionFactory
+    ) {
         $this->orderItemInterfaceFactory = $orderItemInterfaceFactory;
+        $this->orderItemExtensionFactory = $orderItemExtensionFactory;
     }
 
     /**
@@ -36,24 +41,41 @@ class CustomOptionsProcessor
             $extensionAttributes = $productOption->getExtensionAttributes();
             $customOptions = $extensionAttributes ? $extensionAttributes->getCustomOptions() : [];
 
-            return $this->transformOptionsToOrderItems($customOptions);
+            return $this->transformOptionsToOrderItems((int) $item->getItemId(), $customOptions);
         }, $order->getItems(), []);
     }
 
     /**
      * @return OrderItemInterface[]
      */
-    private function transformOptionsToOrderItems(array $customOptions): array
+    private function transformOptionsToOrderItems(int $referenceItemId, array $customOptions): array
     {
-        return reduce(function (array $acc, CustomOptionInterface $item) {
-            return array_merge($acc, $this->transformOptionToOrderItem($item));
+        return reduce(function (array $acc, CustomOptionInterface $item) use ($referenceItemId) {
+            return array_merge($acc, $this->transformOptionToOrderItem($referenceItemId, $item));
         }, $customOptions, []);
     }
 
     /**
      * @return OrderItemInterface[]
      */
-    private function transformOptionToOrderItem(CustomOptionInterface $customOption): array
+    private function transformOptionToOrderItem(int $referenceItemId, CustomOptionInterface $customOption): array
+    {
+        return map(function (array $option) use ($referenceItemId) {
+            $orderItem = $this->orderItemInterfaceFactory->create()
+                ->setItemId(0)
+                ->setSku((string) $option['sku'])
+                ->setName((string) $option['title'])
+                ->setBasePriceInclTax((float) $option['price'])
+                ->setQtyOrdered(1);
+
+            $extensionAttributes = $orderItem->getExtensionAttributes() ?: $this->orderItemExtensionFactory->create();
+            $extensionAttributes->setReferenceItemId($referenceItemId);
+
+            return $orderItem;
+        }, $this->mergeCustomOptionExtraInfo($customOption));
+    }
+
+    private function mergeCustomOptionExtraInfo(CustomOptionInterface $customOption): array
     {
         $extensionAttributes = $customOption->getExtensionAttributes();
 
@@ -62,13 +84,12 @@ class CustomOptionsProcessor
         $optionTitles = explode(self::OPTION_SEPARATOR, $extensionAttributes->getOptionTitle());
         $optionPrices = explode(self::OPTION_SEPARATOR, $extensionAttributes->getOptionPrice());
 
-        return map(function (string $optionTypeId, int $index) use ($optionSkus, $optionTitles, $optionPrices) {
-            return $this->orderItemInterfaceFactory->create()
-                ->setItemId(0)
-                ->setSku((string) $optionSkus[$index])
-                ->setName((string) $optionTitles[$index])
-                ->setBasePriceInclTax((float) $optionPrices[$index])
-                ->setQtyOrdered(1);
-        }, $optionValues);
+        return array_map(function (int $index) use ($optionSkus, $optionTitles, $optionPrices) {
+            return [
+                'sku' => $optionSkus[$index],
+                'title' => $optionTitles[$index],
+                'price' => $optionPrices[$index],
+            ];
+        }, array_keys($optionValues));
     }
 }
