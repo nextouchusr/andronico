369292bb2957e79747f0e885ae50b6250b46eacc
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-04-14 15:36:51 +0200

    fix: remove callbacks

--- app/code/Nextouch/Findomestic/Controller/Payment/Redirect.php
+++ app/code/Nextouch/Findomestic/Controller/Payment/Redirect.php
@@ -6,7 +6,6 @@ namespace Nextouch\Findomestic\Controller\Payment;
 use Magento\Framework\App\Request\Http;
 use Magento\Framework\Exception\LocalizedException;
 use Nextouch\Findomestic\Controller\Payment;
-use Nextouch\Findomestic\Model\Common\Callback;
 use Nextouch\Findomestic\Model\Request\Installment\Create;
 use Nextouch\Quote\Api\Data\CartInterface;
 
@@ -20,8 +19,7 @@ class Redirect extends Payment
             }
 
             $quote = $this->reserveOrderId();
-            $callbacks = $this->getCallbacks();
-            $request = Create::fromDomain($quote, $callbacks);
+            $request = Create::fromDomain($quote);
             $data = $this->installmentManagement->create($request);
 
             return $this->resultJsonFactory->create()->setData(['data' => $data->toArray()]);
@@ -55,18 +53,5 @@ class Redirect extends Payment
         $this->cartRepository->save($quote);
 
         return $quote;
-    }
-
-    private function getCallbacks(): array
-    {
-        return [
-            [
-                'url' => $this->_url->getRouteUrl('/checkout'),
-                'use' => Callback::USE_TYPE_CANCEL,
-                'action' => Callback::ACTION_TYPE_MANUAL,
-                'label' => __('Back to shop')->getText(),
-                'description' => __('Cancel the operation and go back to the shop')->getText(),
-            ],
-        ];
     }
 }
