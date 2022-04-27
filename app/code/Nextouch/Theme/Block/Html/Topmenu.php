df339a5064896d79373b85d2653926b7f2a175c4
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-04-27 10:08:24 +0200

    fix: remove code to change menu level

--- app/code/Nextouch/Theme/Block/Html/Topmenu.php
+++ app/code/Nextouch/Theme/Block/Html/Topmenu.php
@@ -4,55 +4,9 @@ declare(strict_types=1);
 namespace Nextouch\Theme\Block\Html;
 
 use Magento\Framework\Data\Tree\Node;
-use function Lambdish\Phunctional\each;
-use function Lambdish\Phunctional\flat_map;
 
 class Topmenu extends \Magento\Theme\Block\Html\Topmenu
 {
-    public function getHtml($outermostClass = '', $childrenWrapClass = '', $limit = 0): string
-    {
-        $newMenuItems = $this->getNewMenuItems();
-
-        $this->removeAllMenuItems();
-        $this->putNewMenuItems($newMenuItems);
-
-        return parent::getHtml($outermostClass, $childrenWrapClass, $limit);
-    }
-
-    private function getNewMenuItems(): array
-    {
-        $menu = $this->getMenu();
-        $firstLevel = $menu->getChildren();
-
-        return flat_map(function (Node $menuItem) {
-            if ($this->isCategoryMenu($menuItem)) {
-                return $menuItem->getChildren();
-            }
-
-            return $menuItem;
-        }, $firstLevel);
-    }
-
-    private function isCategoryMenu(Node $menuItem): bool
-    {
-        return substr($menuItem->getId(), 0, strlen('category-node-')) === 'category-node-';
-    }
-
-    private function removeAllMenuItems(): void
-    {
-        $menu = $this->getMenu();
-        $firstLevel = $menu->getChildren();
-
-        each(fn(Node $childNode) => $menu->removeChild($childNode), $firstLevel);
-    }
-
-    private function putNewMenuItems(array $newMenuItems): void
-    {
-        $menu = $this->getMenu();
-
-        each(fn(Node $newMenuItem) => $menu->addChild($newMenuItem), $newMenuItems);
-    }
-
     protected function _addSubMenu($child, $childLevel, $childrenWrapClass, $limit): string
     {
         $html = '';
@@ -82,8 +36,8 @@ class Topmenu extends \Magento\Theme\Block\Html\Topmenu
             $item->getPositionClass(),
         ];
 
-        if($item->getLevel() == 0) {
-            $classes[] =  $item->getId();
+        if ($item->getLevel() == 0) {
+            $classes[] = $item->getId();
         }
 
         if ($item->getIsCategory()) {
