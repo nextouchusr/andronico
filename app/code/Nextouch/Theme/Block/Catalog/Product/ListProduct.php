786cf5f4b09957dd3009224809c5d07d02bfcaa9
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-04-27 10:02:51 +0200

    feat: add method to search products by category

--- app/code/Nextouch/Theme/Block/Catalog/Product/ListProduct.php
+++ app/code/Nextouch/Theme/Block/Catalog/Product/ListProduct.php
@@ -4,10 +4,34 @@ declare(strict_types=1);
 namespace Nextouch\Theme\Block\Catalog\Product;
 
 use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ResourceModel\Product\Collection;
+use Magento\Framework\Exception\LocalizedException;
 use function Lambdish\Phunctional\search;
 
 class ListProduct extends \Magento\Catalog\Block\Product\ListProduct
 {
+    private const QUERY_VAR_CATEGORY_ID = 'category_id';
+
+    protected function _getProductCollection()
+    {
+        try {
+            /** @var Collection $collection */
+            $collection = parent::_getProductCollection();
+
+            $rootCategoryId = $this->_storeManager->getStore()->getRootCategoryId();
+            $categoryId = (int) $this->_request->getParam(self::QUERY_VAR_CATEGORY_ID, $rootCategoryId);
+            $category = $this->categoryRepository->get($categoryId);
+            $categoryIds = explode(',', $category->getChildren(true));
+
+            $collection->addCategoriesFilter(['in' => $categoryIds]);
+            $this->setCollection($collection);
+        } catch (LocalizedException $e) {
+            $this->_logger->error($e->getMessage());
+        }
+
+        return $this->_productCollection;
+    }
+
     public function getSavingPercent(Product $product): float
     {
         $price = $product->getPrice();
