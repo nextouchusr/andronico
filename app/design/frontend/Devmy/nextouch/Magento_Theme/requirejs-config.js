a0a3b7773cba1d6ba6a48d91276b891dfc639206
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-09-12 16:54:02 +0200

    feat: add product gallery

--- app/design/frontend/Devmy/nextouch/Magento_Theme/requirejs-config.js
+++ app/design/frontend/Devmy/nextouch/Magento_Theme/requirejs-config.js
@@ -6,7 +6,8 @@ var config = {
         "*": {
             'negativeSlider': 'Magento_Theme/js/negative-slider',
             'initSlick': 'Magento_Theme/js/init-slick',
-            'scrollDecisionTree': "Magento_Theme/js/scroll-decision-tree"
+            'scrollDecisionTree': "Magento_Theme/js/scroll-decision-tree",
+            'lightGallery': 'Magento_Theme/js/lightgallery'
         }
     },
     paths: {
@@ -15,6 +16,9 @@ var config = {
     },
     shim: {
         'negativeSlider': {
+            'deps': ['jquery']
+        },
+        'lightGallery': {
             'deps': ['jquery']
         }
     },
