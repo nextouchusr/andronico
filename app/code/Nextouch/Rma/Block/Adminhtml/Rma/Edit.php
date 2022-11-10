f04e885ddaea04118db986384c7a2be59c891606
Mirko Rapisarda <m.rapisarda@acadevmy.it>
2022-11-10 12:39:12 +0100

    fix: general fixes

--- app/code/Nextouch/Rma/Block/Adminhtml/Rma/Edit.php
+++ app/code/Nextouch/Rma/Block/Adminhtml/Rma/Edit.php
@@ -10,17 +10,29 @@ class Edit extends \Magento\Rma\Block\Adminhtml\Rma\Edit
     protected function _construct(): void
     {
         parent::_construct();
+        $this->addFindomesticRefundButton();
+    }
 
-        if ($this->getRma()->getStatus() === Status::STATE_PROCESSED_CLOSED) {
+    private function addFindomesticRefundButton(): void
+    {
+        if ($this->canAddFindomesticRefundButton()) {
             $this->buttonList->add('send-findomestic-refund', [
                 'label' => __('Send Findomestic Refund'),
                 'class' => 'send-findomestic-refund',
-                'onclick' => 'setLocation(\'' . $this->getSendFindomesticRefundUrl() . '\')',
+                'onclick' => 'setLocation(\'' . $this->getFindomesticRefundUrl() . '\')',
             ], 102);
         }
     }
 
-    public function getSendFindomesticRefundUrl(): string
+    private function canAddFindomesticRefundButton(): bool
+    {
+        $isFindomestic = $this->getRma()->getOrder()->getPayment()->getMethod() === 'findomestic_paymentservice';
+        $isClosed = $this->getRma()->getStatus() === Status::STATE_PROCESSED_CLOSED;
+
+        return $isFindomestic && $isClosed;
+    }
+
+    private function getFindomesticRefundUrl(): string
     {
         return $this->getUrl(
             'adminhtml/*/sendFindomesticRefund',
